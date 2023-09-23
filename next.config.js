44005d329aca8a44ffe3972307874b91c725d00b
Kevin Nemeržitski <2716421+knemerzitski@users.noreply.github.com>
2023-09-23 13:50:28 +0300

    ci: 🎡 switch to ssg export

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Enable Static Site Generation
   reactStrictMode: true,
+  trailingSlash: false,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
   webpack(config) {
     // Load GraphQL Files
     config.module.rules.push({
